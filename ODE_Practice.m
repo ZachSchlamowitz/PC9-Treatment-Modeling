abd2fa5fb56701851c1516cd8cfbfdd4e1c7a444
Zach Schlamowitz <zschlamowitz@email.arizona.edu>
2021-07-07 13:00:58 -0700

    Added ODE practice file and .gitignore

--- ODE_Practice.m
+++ ODE_Practice.m
@@ -32,35 +32,62 @@
 %% Ex.3: Lotka-Volterra (from documentation 'Solve Predator-Prey Equations')
 % x' = x - axy
 % y' = -y + Bxy
+% 
+% t0 = 0;
+% tf = 15;
+% initvals = [20; 20];
+% [time, sol] = ode23(@lotka, [t0 tf], initvals);
+% 
+% % Time Series Plot
+% plot(time, sol)
+% title('Predator, Prey Populations Over Time')
+% xlabel('t')
+% ylabel('Population')
+% legend('Prey', 'Predators', 'Location', 'North')
+% 
+% % Phase Plane Plot
+% plot(sol(:,1), sol(:,2))
+% title('Phase Space')
+% xlabel('Prey Population')
+% ylabel('Predator Population')
+% 
+% % Add in version with ode45 as well
+% [T,S] = ode45(@lotka, [t0 tf], initvals);
+% plot(sol(:,1), sol(:,2), '-', S(:,1), S(:,2), '-');
+% title('Comparing Phase Plane Portraits')
+% legend('ode23', 'ode45')
+% 
+% function eqs = lotka(t,initvals)
+%     a = 0.01;
+%     B = 0.02;
+%     eqs = zeros(2,1);
+%     eqs(1) = initvals(1) - a*initvals(1)*initvals(2);
+%     eqs(2) = -initvals(2) + B*initvals(1)*initvals(2);
+% end
 
-t0 = 0;
-tf = 15;
-initvals = [20; 20];
-[time, sol] = ode23(@lotka, [t0 tf], initvals);
+%% Ex.4: My Own Creation, an autonomous, nonlinear system
+% x' = 3x + x^2 + xy
+% y' = xy + 2y^2
 
-% Time Series Plot
-plot(time, sol)
-title('Predator, Prey Populations Over Time')
-xlabel('t')
-ylabel('Population')
-legend('Prey', 'Predators', 'Location', 'North')
-
-% Phase Plane Plot
-plot(sol(:,1), sol(:,2))
-title('Phase Space')
-xlabel('Prey Population')
-ylabel('Predator Population')
-
-% Add in version with ode45 as well
-[T,S] = ode45(@lotka, [t0 tf], initvals);
-plot(sol(:,1), sol(:,2), '-', S(:,1), S(:,2), '-');
-title('Comparing Phase Plane Portraits')
-legend('ode23', 'ode45')
-
-function eqs = lotka(t,initvals)
-    a = 0.01;
-    B = 0.02;
-    eqs = zeros(2,1);
-    eqs(1) = initvals(1) - a*initvals(1)*initvals(2);
-    eqs(2) = -initvals(2) + B*initvals(1)*initvals(2);
-end
+% tspan = [0 25];
+% initconds = [3; 0.5];
+% [time, sol] = ode23(@equ, tspan, initconds);
+% 
+% % Plot Time Series
+% plot(time, sol, '-o')
+% legend('People', 'Martians')
+% title('Example ODE System')
+% xlabel('time')
+% ylabel('Population')
+% 
+% % Plot Phase Plane
+% plot(sol(:,1), sol(:,2))
+% title('Phase Plane')
+% xlabel('People')
+% ylabel('Martians')
+% 
+% function z = equ(t,z0)
+%     z = zeros(2,1);
+%     z(1) = 3*z0(1) - z0(1)^2 - z0(1)*z0(2);
+%     z(2) = -z0(1)*z0(2) - 2*z0(2)^2;
+% end
