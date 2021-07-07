1697090397c1414117a3d0355bac4e63c4a2e0a7
Zach Schlamowitz <zschlamowitz@email.arizona.edu>
2021-07-07 13:03:02 -0700

    Created function file, got model to run
    Moved ODE functions and all other parameter functions to their own file, pc9treat.m. Rearranged main file in so doing. Model now outputs without error but output is grossly incorrect.

--- Pc9_ODE_Model.m
+++ Pc9_ODE_Model.m
@@ -8,45 +8,65 @@ t_E = NaN; % hour of dosing with osimertinib; =NaN when not adding drug
 E_0 = 10;  % Initial concentration of osimertinib dose (uM)
 beta = 1/13; % Fraction of S/G2 cells which enter mitosis in a given hour
 
-%% (Exaggerated) Parameter functions
-% Structs for viabilty Hill curve parameters
+% %% (Exaggerated) Parameter functions
+% % Structs for viabilty Hill curve parameters
 v.E0 = 95;  % effect (viability) in absence of drug (percentage)
 v.Einf = 5; % maximum possible effect (viability) with drug (percentage)
 v.EC50 = 2; % effect (viability) of drug at half max efficacy concentration of drug (percentage)
 v.hill = 5; % Hill slope coefficient
 r = v;  % NOTE: here MATLAB creates separate copy of struct rather than an alias
-
-% Viability Hill curves for Vulnerable, Resistant populations
-syms viability_V(C_E) viability_R(C_E)
-viability_V(C_E) = v.E0 + (v.Einf - v.E0)/(1 + (v.EC50/C_E)^v.hill);
-viability_R(C_E) = v.E0 + (v.Einf - v.E0)/(1 + (2*v.EC50/C_E)^v.hill); % resistant population has twice the EC50 of vulnerable population
-
-syms alpha(C_p) d_V(C_E) d_R(C_E)
-alpha(C_p) = 0.85 - 0.07*C_p; % progression (G1 --> S/G2) fraction 
-d_V(C_E) = 100 - viabilty_V(C_E); % death fraction of vulnerable (G1) cells
-d_R(C_E) = 100 - viabilty_R(C_E); % death fraction of resistant (S/G2) cells
+% 
+% % Viability Hill curves for Vulnerable, Resistant populations
+% syms viability_V(C_E) viability_R(C_E)
+% viability_V(C_E) = v.E0 + (v.Einf - v.E0)/(1 + (v.EC50/C_E)^v.hill);
+% viability_R(C_E) = v.E0 + (v.Einf - v.E0)/(1 + (2*v.EC50/C_E)^v.hill); % resistant population has twice the EC50 of vulnerable population
+% 
+% syms alpha(C_p) d_V(C_E) d_R(C_E)
+% alpha(C_p) = 0.85 - 0.07*C_p; % progression (G1 --> S/G2) fraction 
+% d_V(C_E) = 100 - viability_V(C_E); % death fraction of vulnerable (G1) cells
+% d_R(C_E) = 100 - viability_R(C_E); % death fraction of resistant (S/G2) cells
+% 
+% 
+% %% FIRST ATTEMPT (FAILED/INCOMPLETE):
+% syms C_p(t) C_E(t)
+% % ode1 = diff(V,t) == -alpha*V - d_V*V + 2*beta*R;
+% % ode2 = diff(R,t) ==  alpha*V - d_R*R -   beta*R;
+% % % C_p(t) = piecewise(t<t_p, 0, t>t_p, P_0*(1/2)^(t-27));
+% C_p(t) = 0;
+% % % C_E(t) = piecewise(t<t_E, 0, t>t_E, E_0*(1/2)^(t-48));
+% C_E(t) = 0;
+% % 
+% % % Specify initial conditions
+% % cond1 = V(0) == 40; 
+% % cond2 = R(0) == 60;
+% % 
+% % % Solve odes
+% % odes = [ode1; ode2];
+% % conds = [cond1; cond2];
+% % [Vsol(t), Rsol(t)] = dsolve(odes, conds)  %z: Andrew suggests using ode23 rather than dsolve. This uses numerical approach.
+% % 
+% 
+% 
+% 
+% % assert(d_V>0, 'death fraction d_V cannot be negative')
+% % assert(d_R>0, 'death fraction d_R cannot be negative')
+% % assert(alpha>0, 'progression fraction alpha cannot be negative')
 
 %% Main equations
-syms V(t) R(t) C_p(t) C_E(t)
-ode1 = diff(V,t) == -alpha*V - d_V*V + 2*beta*R;
-ode2 = diff(R,t) ==  alpha*V - d_R*R -   beta*R;
-% C_p(t) = piecewise(t<t_p, 0, t>t_p, P_0*(1/2)^(t-27));
-C_p(t) = 0;
-% C_E(t) = piecewise(t<t_E, 0, t>t_E, E_0*(1/2)^(t-48));
-C_E(t) = 0;
 
-% Specify initial conditions
-cond1 = V(0) == 40; 
-cond2 = R(0) == 60;
+tspan = [0 24];
+initconds = [40; 60];
+[time, state] = ode23(@pc9treat, tspan, initconds)
 
-% Solve odes
-odes = [ode1; ode2];
-conds = [cond1; cond2];
-[Vsol(t), Rsol(t)] = dsolve(odes, conds)  %z: Andrew suggests using ode23 rather than dsolve. This uses numerical approach.
+% Plot
+plot(time, state, '-o')
+title('Vulnerable and Resistant Populations Over Time')
+xlabel('Time (hrs)')
+ylabel('Gross Population')
+legend('Vulnerable', 'Resistant')
 
-
-
-
-assert(d_V>0, 'death fraction d_V cannot be negative')
-assert(d_R>0, 'death fraction d_R cannot be negative')
-assert(alpha>0, 'progression fraction alpha cannot be negative')
+% function state = pc9treat(t,S0)
+%     state = zeros(2,1);
+%     state(1) = -alpha*S0(1) - d_V*S0(1) + 2*beta*S0(2);
+%     state(2) =  alpha*S0(1) - d_R*S0(2) -   beta*S0(2);
+% end
---
+++ pc9treat.m
@@ -0,0 +1,66 @@
+% Functions File for Pc9_ODE_Model
+% Author: Zach Schlamowitz (7/7/21)
+function state = pc9treat(t,S0)
+    t_p = NaN; % hour of dosing with palbociclib; =NaN when not adding drug
+    P_0 = 10;  % Initial concentration of palbociclib dose (uM)
+    t_E = NaN; % hour of dosing with osimertinib; =NaN when not adding drug
+    E_0 = 10;  % Initial concentration of osimertinib dose (uM)
+    beta = 1/13; % Fraction of S/G2 cells which enter mitosis in a given hour
+
+    C_p = palbo(t, t_p, P_0);
+    alp = growth(C_p);
+    
+    C_E = osi(t, t_E, E_0);
+    d_V = death_V(C_E);
+    d_R = death_R(C_E);
+    
+    state = zeros(2,1);
+    state(1) = -alp*S0(1) - d_V*S0(1) + 2*beta*S0(2);
+    state(2) =  alp*S0(1) - d_R*S0(2) -   beta*S0(2);
+end
+
+
+% Viability Hill curves for Vulnerable, Resistant populations
+function via_V = viability_V(C_E)
+    % Viability Hill Curve Parameters
+    v.E0 = 95;  % effect (viability) in absence of drug (percentage)
+    v.Einf = 5; % maximum possible effect (viability) with drug (percentage)
+    v.EC50 = 2; % effect (viability) of drug at half max efficacy concentration of drug (percentage)
+    v.hill = 5; % Hill slope coefficient
+    
+    via_V = v.E0 + (v.Einf - v.E0)/(1 + (v.EC50/C_E)^v.hill);
+end
+function via_R = viability_R(C_E)
+    % Viability Hill Curve Parameters
+    r.E0 = 95;  % effect (viability) in absence of drug (percentage)
+    r.Einf = 5; % maximum possible effect (viability) with drug (percentage)
+    r.EC50 = 2; % effect (viability) of drug at half max efficacy concentration of drug (percentage)
+    r.hill = 5; % Hill slope coefficient
+
+    via_R = r.E0 + (r.Einf - r.E0)/(1 + (2*r.EC50/C_E)^r.hill); % resistant population has twice the EC50 of vulnerable population
+end
+
+function alp = growth(C_p)
+    alp = 0.85 - 0.07*C_p; % progression (G1 --> S/G2) fraction 
+end
+function d_V = death_V(C_E)
+    d_V = 100 - viability_V(C_E); % death fraction of vulnerable (G1) cells
+end
+function d_R = death_R(C_E)
+    d_R = 100 - viability_R(C_E); % death fraction of resistant (S/G2) cells
+end
+
+function C_p = palbo(t, t_p, P_0)
+    if t<t_p
+        C_p = 0;
+    else
+        C_p = P_0*(1/2)^(t-27);
+    end
+end
+function C_E = osi(t, t_E, E_0)
+    if t<t_E
+        C_E = 0;
+    else
+        C_E = E_0*(1/2)^(t-48);
+    end
+end
