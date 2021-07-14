a251c8d3e61a202e28bde9557130f4eef20c67b0
Zach Schlamowitz <zschlamowitz@email.arizona.edu>
2021-07-13 17:20:03 -0700

    Calibrated parameter equations to be hourly rates
    Used Andrew's suggested values as guideline to scale parameter equations to hourly changes. Adjusted beta value to get resistant population greater than vulnerable population. Doubling time now very close to 24 hours.

--- pc9treat.m
+++ pc9treat.m
@@ -1,11 +1,11 @@
 % Functions File for Pc9_ODE_Model
 % Author: Zach Schlamowitz (7/7/21)
-function state = pc9treat(t,S0)
+function dstate_dt = pc9treat(t,S0)
     t_p = NaN; % hour of dosing with palbociclib; =NaN when not adding drug
-    P_0 = 10;  % Initial concentration of palbociclib dose (uM)
+    P_0 = 5;  % Initial concentration of palbociclib dose (uM)
     t_E = NaN; % hour of dosing with osimertinib; =NaN when not adding drug
-    E_0 = 10;  % Initial concentration of osimertinib dose (uM)
-    beta = 1/13; % Fraction of S/G2 cells which enter mitosis in a given hour
+    E_0 = 5;  % Initial concentration of osimertinib dose (uM)
+    beta = 0.055;%1/13; % Fraction of S/G2 cells which enter mitosis in a given hour
 
     C_p = palbo(t, t_p, P_0);
     alp = growth(C_p);
@@ -13,10 +13,11 @@ function state = pc9treat(t,S0)
     C_E = osi(t, t_E, E_0);
     d_V = death_V(C_E);
     d_R = death_R(C_E);
-    
-    state = zeros(2,1);
-    state(1) = -alp*S0(1) - d_V*S0(1) + 2*beta*S0(2);
-    state(2) =  alp*S0(1) - d_R*S0(2) -   beta*S0(2);
+%     d_V = 0.002083;
+%     d_R = d_V;
+    dstate_dt = zeros(2,1);
+    dstate_dt(1) = -alp*S0(1) - d_V*S0(1) + 2*beta*S0(2);
+    dstate_dt(2) =  alp*S0(1) - d_R*S0(2) -   beta*S0(2);
 end
 
 
@@ -42,13 +43,13 @@ function via_R = viability_R(C_E)
 end
 
 function alp = growth(C_p)
-    alp = 0.85 - 0.07*C_p; % progression (G1 --> S/G2) fraction 
+    alp = 1/11 - 0.01*C_p; % progression (G1 --> S/G2) rate (fraction leaving per hour)
 end
 function d_V = death_V(C_E)
-    d_V = (100 - viability_V(C_E))/100; % death fraction of vulnerable (G1) cells
+    d_V = (100 - viability_V(C_E))/(100*72); % death fraction of vulnerable (G1) cells per hour
 end
 function d_R = death_R(C_E)
-    d_R = (100 - viability_R(C_E))/100; % death fraction of resistant (S/G2) cells
+    d_R = (100 - viability_R(C_E))/(100*72); % death fraction of resistant (S/G2) cells per hour
 end
 
 function C_p = palbo(t, t_p, P_0)
