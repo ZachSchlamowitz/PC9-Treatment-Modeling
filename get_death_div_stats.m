dbdefba690b77ff0de5c4e46f6272017329d6798
Zach Schlamowitz <zschlamowitz@email.arizona.edu>
2021-07-26 18:45:56 -0700

    Added Division Stats Automation

--- get_death_div_stats.m
+++ get_death_div_stats.m
@@ -71,7 +71,7 @@ for k=1:numel(datasets)
         invalid_deaths = 0;
         for i=1:numel(num_deaths_per_cell)
             if num_deaths_per_cell(i) > 1
-                fprintf('flag cell ')
+                fprintf('death flag cell ')
                 fprintf(num2str(i))
                 fprintf('\n')
                 death_flags(i) = 1;
@@ -90,9 +90,9 @@ for k=1:numel(datasets)
     valid_death_cells_72hrs = numel(subset_stats(1).num_deaths_per_cell) - sum(subset_stats(1).death_flags);
     prob_death_72hrs = total_deaths_72hrs / valid_death_cells_72hrs;
     
-    total_deaths_24hrs = sum(subset_stats(2).num_deaths_per_cell) - subset_stats(2).invalid_deaths;
+    total_divs_24hrs = sum(subset_stats(2).num_deaths_per_cell) - subset_stats(2).invalid_deaths;
     valid_death_cells_24hrs = numel(subset_stats(2).num_deaths_per_cell) - sum(subset_stats(2).death_flags);
-    prob_death_24hrs = total_deaths_24hrs / valid_death_cells_24hrs;
+    prob_death_24hrs = total_divs_24hrs / valid_death_cells_24hrs;
     
     output_structs(k).num_cells = numel(subset_stats(1).num_deaths_per_cell);
     output_structs(k).death_events_per_cell = subset_stats(1).num_deaths_per_cell;
@@ -100,12 +100,64 @@ for k=1:numel(datasets)
     output_structs(k).num_deaths_72hrs = total_deaths_72hrs;
     output_structs(k).prob_death_72hrs =  prob_death_72hrs;
     output_structs(k).num_cells_good4death_24hrs = valid_death_cells_24hrs;
-    output_structs(k).num_deaths_24hrs = total_deaths_24hrs;
+    output_structs(k).num_deaths_24hrs = total_divs_24hrs;
     output_structs(k).prob_death_24hrs =  prob_death_24hrs;
 end
 
 
 % %% Division
+for k=1:numel(datasets)
+    
+    full_72 = struct('divisionMatrix',datasets(k).divisionMatrixDataset);
+    first_24 = struct('divisionMatrix',datasets(k).divisionMatrixDataset(:,1:(24*4)));  % Death in first 24 hours
+    data_subsets = [full_72 first_24];
+    subset_stats = [struct() struct()];
+    
+    for j=1:numel(data_subsets)
+        num_div_events_per_cell = sum(data_subsets(j).divisionMatrix,2);
+        div_flags = zeros(numel(num_divs_per_cell),1);
+        invalid_div_events = 0;
+        for i=1:numel(num_div_events_per_cell)
+            if mod(num_div_events_per_cell(i),2) ~= 0 % odd number of div events
+                fprintf('div flag cell ')
+                fprintf(num2str(i))
+                fprintf('\n')
+                div_flags(i) = 1;
+                invalid_div_events = invalid_div_events + num_div_events_per_cell(i);
+            end
+        end
+        % Store stats computed for this sub-dataset (either first 24 or all
+        % 72 hrs) of this dose of gefitnib
+        subset_stats(j).num_div_events_per_cell = num_div_events_per_cell;
+        subset_stats(j).div_flags = div_flags;
+        subset_stats(j).invalid_div_events = invalid_div_events;
+        
+    end
+
+    total_divs_72hrs = (sum(subset_stats(1).num_div_events_per_cell) - subset_stats(1).invalid_div_events) / 2; % scalar
+    valid_div_cells_72hrs = numel(subset_stats(1).num_div_events_per_cell) - sum(subset_stats(1).div_flags); % scalar
+    avg_divs_per_cell_72 = total_divs_72hrs / valid_div_cells_72hrs; % scalar
+    avg_divs_per_cell_per_hr_72 = avg_divs_per_cell_72/72; % scalar
+    hrs_per_div_72 = 1/avg_divs_per_cell_per_hr_72; % scalar; number of hours required for one cell to copmlete one division (one cell cycle)
+    
+    total_divs_24hrs = (sum(subset_stats(2).num_div_events_per_cell) - subset_stats(2).invalid_div_events) / 2;
+    valid_div_cells_24hrs = numel(subset_stats(2).num_div_events_per_cell) - sum(subset_stats(2).div_flags);
+    avg_divs_per_cell_24 = total_divs_24hrs / valid_div_cells_24hrs; % scalar
+    avg_divs_per_cell_per_hr_24 = avg_divs_per_cell_24/24; % scalar
+    hrs_per_div_24 = 1/avg_divs_per_cell_per_hr_24; % scalar; number of hours required for one cell to copmlete one division (one cell cycle) in first 24 hours
+    
+    output_structs(k).num_cells = numel(subset_stats(1).num_div_events_per_cell);
+    output_structs(k).div_events_per_cell = subset_stats(1).num_div_events_per_cell;
+    output_structs(k).num_cells_good4divs_72hrs = valid_div_cells_72hrs;
+    output_structs(k).num_divs_72hrs = total_divs_72hrs;
+    output_structs(k).hrs_per_div_72 = hrs_per_div_72;
+    output_structs(k).num_cells_good4divs_24hrs = valid_div_cells_24hrs;
+    output_structs(k).num_divs_24hrs = total_divs_24hrs;
+    output_structs(k).hrs_per_div_24 = hrs_per_div_24;
+
+end
+
+
 % num_divs_per_cell = sum(Clean_Gef0_05_200cells.divisionMatrixDataset,2);
 % div_flags = zeros(numel(num_divs_per_cell),1);
 % for i=1:numel(num_divs_per_cell)
