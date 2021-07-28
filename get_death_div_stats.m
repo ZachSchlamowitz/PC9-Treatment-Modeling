cd71e62109da7151e7b90043dfb73379901a5275
Zach Schlamowitz <zschlamowitz@email.arizona.edu>
2021-07-27 22:44:13 -0700

    Fixed one bug with automation. Added comments and visual spacers.

--- get_death_div_stats.m
+++ get_death_div_stats.m
@@ -1,4 +1,29 @@
+%% get_death_div_stats.m
+% This script obtains statistics regarding the death and division 
+% rates/probabilities of cells (e.g., pc-9 non-small cell lung cancer
+% cells) by analyzing their single-cell trace data. This data is to be 
+% inputted in a matlab struct via a command:
+% e.g.) >>    load('CleanGef0_1_200cells');
+% Furthermore, it should then be added as a column to the struct matrix
+% 'datasets':
+% e.g.) >>    datasets = [MyOtherData CleanGef0_1_200cells Etc.];
+% Note that the name of the struct variable that goes in this matrix may
+% not default to the name of the .mat file which houses it. Verify this by
+% opening the file manually. 
 
+% Finally, it should also be noted that this script outputs statistics for
+% both 72 and 24 hour segments of a 72 hour movie. However, to ensure
+% accurate data, we ignore any cells with an odd number of divisions in the
+% time period. This means that an overly high number of cells are ignored
+% in the 24 hour case, as many have begun divisions but not completed them
+% at hour 24. This may skew that statistic somewhat; keep this in mind.
+
+% To only run death or division stats, comment out the other, undesired for
+% loop. Or, if you're really feeling it, add in an if-else settings switch.
+
+% Author: Zach Schlamowitz, 7/27/21
+
+%% Initialization
 % Initialize Output Structs
 % gef_stats_0p1uM = struct();
 gef_stats_0p5uM = struct();
@@ -11,51 +36,7 @@ load('Clean_Gef0_05_174cells');
 
 datasets = [Clean_Gef0_05_200cells]; % [CleanGef0_1]
 
-% % For 1uM Gefitnib:
-% num_divs_per_cell = sum(Clean_Gef_1uM_200cells.divisionMatrixDataset,2);
-% div_flags = zeros(numel(num_divs_per_cell),1);
-% for i=1:numel(num_divs_per_cell)
-%     if mod(num_divs_per_cell(i),2) ~= 0
-%         fprintf('flag cell ')
-%         fprintf(num2str(i))
-%         fprintf('\n')
-%         div_flags(i) = 1;
-%     end
-% end
-% 
-% total_divs = sum(num_divs_per_cell);
-% for i=[1 18 22 26 41 48 66 125 126]
-%     total_divs = total_divs - num_divs_per_cell(i);
-% end
-% 
-% gef_stats_1uM.div_events_per_cell = num_divs_per_cell;
-% gef_stats_1uM.div_flags = div_flags;
-% gef_stats_1uM.num_divs = total_divs/2;
-% gef_stats_1uM.prob_div = gef_stats_1uM.num_divs/gef_stats_1uM.num_cells_good4divs
-% 
-% % For 0.1uM Gefitnib:
-% num_divs_per_cell = sum(CleanGef0_1.divisionMatrixDataset,2);
-% div_flags = zeros(numel(num_divs_per_cell),1);
-% for i=1:numel(num_divs_per_cell)
-%     if mod(num_divs_per_cell(i),2) ~= 0
-%         fprintf('flag cell ')
-%         fprintf(num2str(i))
-%         fprintf('\n')
-%         div_flags(i) = 1;
-%     end
-% end
-% 
-% total_divs = sum(num_divs_per_cell);
-% for i=[33 67 68 134 165]
-%     total_divs = total_divs - num_divs_per_cell(i);
-% end
-% 
-% gef_stats_0p1uM.div_events_per_cell = num_divs_per_cell;
-% gef_stats_0p1uM.div_flags = div_flags;
-% gef_stats_0p1uM.num_divs = total_divs/2;
-% gef_stats_0p1uM.prob_div = gef_stats_0p1uM.num_divs/gef_stats_0p1uM.num_cells_good4divs
-% 
-% For 0.5uM Gefitnib:
+
 %% Death
 % Loop over relevant datasets and run statistic extraction algorithm
 for k=1:numel(datasets)
@@ -83,9 +64,16 @@ for k=1:numel(datasets)
         subset_stats(j).num_deaths_per_cell = num_deaths_per_cell;
         subset_stats(j).death_flags = death_flags;
         subset_stats(j).invalid_deaths = invalid_deaths;
-        
+    
+    % Visual spacer
+    if invalid_deaths > 0
+        fprintf('\n')   
     end
-
+    
+    end
+    
+    
+    
     total_deaths_72hrs = sum(subset_stats(1).num_deaths_per_cell) - subset_stats(1).invalid_deaths;
     valid_death_cells_72hrs = numel(subset_stats(1).num_deaths_per_cell) - sum(subset_stats(1).death_flags);
     prob_death_72hrs = total_deaths_72hrs / valid_death_cells_72hrs;
@@ -105,7 +93,7 @@ for k=1:numel(datasets)
 end
 
 
-% %% Division
+%% Division
 for k=1:numel(datasets)
     
     full_72 = struct('divisionMatrix',datasets(k).divisionMatrixDataset);
@@ -115,7 +103,7 @@ for k=1:numel(datasets)
     
     for j=1:numel(data_subsets)
         num_div_events_per_cell = sum(data_subsets(j).divisionMatrix,2);
-        div_flags = zeros(numel(num_divs_per_cell),1);
+        div_flags = zeros(numel(num_div_events_per_cell),1);
         invalid_div_events = 0;
         for i=1:numel(num_div_events_per_cell)
             if mod(num_div_events_per_cell(i),2) ~= 0 % odd number of div events
@@ -131,9 +119,14 @@ for k=1:numel(datasets)
         subset_stats(j).num_div_events_per_cell = num_div_events_per_cell;
         subset_stats(j).div_flags = div_flags;
         subset_stats(j).invalid_div_events = invalid_div_events;
-        
+     
+    % Visual spacer
+    if invalid_div_events > 0
+        fprintf('\n')   
     end
-
+    
+    end
+    
     total_divs_72hrs = (sum(subset_stats(1).num_div_events_per_cell) - subset_stats(1).invalid_div_events) / 2; % scalar
     valid_div_cells_72hrs = numel(subset_stats(1).num_div_events_per_cell) - sum(subset_stats(1).div_flags); % scalar
     avg_divs_per_cell_72 = total_divs_72hrs / valid_div_cells_72hrs; % scalar
@@ -156,29 +149,3 @@ for k=1:numel(datasets)
     output_structs(k).hrs_per_div_24 = hrs_per_div_24;
 
 end
-
-
-% num_divs_per_cell = sum(Clean_Gef0_05_200cells.divisionMatrixDataset,2);
-% div_flags = zeros(numel(num_divs_per_cell),1);
-% for i=1:numel(num_divs_per_cell)
-%     if mod(num_divs_per_cell(i),2) ~= 0
-%         fprintf('flag cell ')
-%         fprintf(num2str(i))
-%         fprintf('\n')
-%         div_flags(i) = 1;
-%     end
-% end
-% 
-% total_divs = sum(num_divs_per_cell);
-% for i=[33 67 68 134 165]
-%     total_divs = total_divs - num_divs_per_cell(i);
-% end
-% 
-% gef_stats_0p1uM.div_events_per_cell = num_divs_per_cell;
-% gef_stats_0p1uM.div_flags = div_flags;
-% gef_stats_0p1uM.num_divs = total_divs/2;
-% gef_stats_0p1uM.prob_div = gef_stats_0p1uM.num_divs/gef_stats_0p1uM.num_cells_good4divs
-% 
-
-
-
