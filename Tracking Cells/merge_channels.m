92be2d5bb6aa1171c062a2efdfdea4d23f2feb17
Zach Schlamowitz <zschlamowitz@email.arizona.edu>
2021-08-13 18:05:17 -0700

    Updated for harddrive

--- Tracking Cells/merge_channels.m
+++ Tracking Cells/merge_channels.m
@@ -6,15 +6,16 @@
 % Author: Zach Schlamowitz (8/10/20)
 
 %% Main
-files_folder = 'F:\2021-07-26 Partial Copy\RAW_DATA';
-cd 'F:\2021-07-26 Partial Copy\RAW_DATA'
+cd 'D:\Zach\2021-07-26\RAW_DATA'  % point MATLAB to the RAW_DATA folder contained desired tifs
+    % From flashdrive (Seagull): 'F:\2021-07-26 Partial Copy\RAW_DATA'
+    % From Lisa Shanks' Harddrive (Seagate Expansion Desk): 'D:\Zach\2021-07-26\RAW_DATA'
 date = '2021-07-26-';
 channel_a = 1; % channel numbers for desired channels
 channel_b = 3;
 num_positions = 120;
-num_timepoints = 282; % number of timepoints for one channel at one position
+num_timepoints = 289; % number of timepoints for one channel at one position
 
-for xy = 33:35
+for xy = 1:num_positions
     for t=1:num_timepoints
         filename_a = horzcat(date,'xy',sprintf('%03d',xy),'c',num2str(channel_a),'t',sprintf('%03d',t));
         filename_b = horzcat(date,'xy',sprintf('%03d',xy),'c',num2str(channel_b),'t',sprintf('%03d',t));
