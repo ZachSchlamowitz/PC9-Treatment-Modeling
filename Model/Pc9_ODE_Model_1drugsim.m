c67cdebd6df0f7d81204f0fddd6dc0481d7b52cd
Zach Schlamowitz <zschlamowitz@email.arizona.edu>
2021-08-16 19:07:57 -0700

    Updated Sim Model to Run

--- Model/Pc9_ODE_Model_1drugsim.m
+++ Model/Pc9_ODE_Model_1drugsim.m
@@ -4,10 +4,10 @@
 
 %% Main equations
 
-tspan = [0 240];
-% tspan = [0:72];
+% tspan = [0 240];
+tspan = [0:72];
 initconds = [40; 60];
-[time, state] = ode23(@pc9treat, tspan, initconds);
+[time, state] = ode23(@pc9treat_sim, tspan, initconds);
 state(:,3) = state(:,1) + state(:,2);  % total population
 
 % Get hourly growth rate, derivative values at each hour
@@ -15,7 +15,7 @@ state_derivs = zeros(size(state,1),2);  % initialize
 growth_rate  = zeros(size(state,1),2);
 for t = 1:size(time)
     s0 = state(t,1:2);  % update initial condition
-    dstate_dt = pc9treat(t,s0);  % get derivatives
+    dstate_dt = pc9treat_sim(t,s0);  % get derivatives
     state_derivs(t,:) = dstate_dt;  % store derivatives
 
     for col = 1:2
@@ -32,20 +32,21 @@ end
 % end
 
 % Plot
+figure('Name','PC9 ODE Model SIM Time Series')
 plot(time, state, '-o')
 title('Vulnerable and Resistant Populations Over Time')
 xlabel('Time (hrs)')
 ylabel('Gross Population')
 legend('Vulnerable', 'Resistant', 'Total Population')
-xlim([-10 106])
+% xlim([-10 106])
 ylim([-10 350])
 
 % Plot Phase Plane
-figure
-plot(state(:,1), state(:,2))
-title('Phase Plane Portrait')
-xlabel('Vulnerable Population')
-ylabel('Resistant Population')
+% figure('Name','PC9 ODE Model SIM Phase Space')
+% plot(state(:,1), state(:,2))
+% title('Phase Plane Portrait')
+% xlabel('Vulnerable Population')
+% ylabel('Resistant Population')
 % xlim([-10 110])
 % ylim([-10 110])
 
